2b30c6e7099e33f2cd38ee10bec19e39d83a6726
Pedro Borges <pedro.borges@rethink.dev>
2021-09-11 17:50:07 -0300

    fix: removing unused vars

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-import { BRASIL_IO_API_TOKEN } from '~/constants';
-
 const URLs = {
   world: 'https://covid19-brazil-api.now.sh',
   cities: 'https://api.brasil.io/dataset/covid19/caso/data/',
